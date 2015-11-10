cb41708b83974b9e4c1dc2de0b9a1bfa582a93fa
Ross McDonald <ross@influxdb.com>
2015-11-09 18:15:26 -0600

    Fixing syntax issues in pre-install script (Im too familiar with bash).

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,7 +3,7 @@ set -x
 
 # Copy existing configuration if pre-existing installation is found
 test -f /etc/opt/influxdb/influxdb.conf
-if [[ $? -eq 0 ]]; then
+if [ $? -eq 0 ]; then
     cp -a /etc/opt/influxdb /etc/influxdb
 fi
 
