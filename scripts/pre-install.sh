c2ada5c4344c0bcbfc155a600aea00a0c5cd0b89
Ross McDonald <ross@influxdb.com>
2015-11-09 18:13:15 -0600

    Modifying pre install script due to weird errors being thrown on install.

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,6 +2,9 @@
 set -x
 
 # Copy existing configuration if pre-existing installation is found
-test -f /etc/opt/influxdb/influxdb.conf && cp -a /etc/opt/influxdb /etc/influxdb
+test -f /etc/opt/influxdb/influxdb.conf
+if [[ $? -eq 0 ]]; then
+    cp -a /etc/opt/influxdb /etc/influxdb
+fi
 
 exit
